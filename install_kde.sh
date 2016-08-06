aa7fc6ca46bdedb71d8cf2e3edb495c23d270726
Alex Sahm <alexander.sahm@gmx.de>
2016-08-06 12:26:57 +0200

    added shell scripts: install_kde, install_kde, install_kde

---
+++ install_kde.sh
@@ -0,0 +1,2 @@
+#sudo apt-get install kde-standard
+sudo apt-get install language-pack-kde-de
---
+++ install_mate.sh
@@ -0,0 +1,5 @@
+sudo apt-add-repository ppa:ubuntu-mate-dev/ppa
+sudo apt-add-repository ppa:ubuntu-mate-dev/trusty-mate
+sudo apt-get update
+sudo apt-get upgrade 
+sudo apt-get install --no-install-recommends ubuntu-mate-core ubuntu-mate-desktop 
