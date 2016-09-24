edd80e0000970655c709a5cf84dea339a2b2fcc4
Alex Sahm <alexander.sahm@gmx.de>
2016-09-24 16:45:09 +0200

    added log function

--- mylib.sh
+++ mylib.sh
@@ -3,13 +3,25 @@
 filename="mylib.sh"
 
 function execCmd() {
-  mn="execCmd()"
+  fn="execCmd()"
   command2exec=$@
-  echo "$filename - $mn - Execute command '$command2exec' now..."
+  log $filename $fn "INFO" "Execute command '$command2exec' now..."
   if  eval $command2exec ; then 
-     echo "$filename - $mn - Command '$command2exec' executed successfully"
+     log $filename $fn "INFO" "Command '$command2exec' executed successfully"
   else
-     echo "$filename - $mn - '$command2exec' failed with rc: $?"
+     log $filename $fn "ERROR" "Command '$command2exec' failed with rc: $?"
      exit $?
   fi
 }
+
+function log() {
+   fileName=$1
+   functionName=$2
+   logLevel=$3
+   logMessage=$4
+   day=$(date +%F)
+   time=$(date +%T)
+   secSince1970=$(date +%s)
+   echo $secSince1970 $day $time $logLevel $fileName $functionName - $logMessage
+}
+
