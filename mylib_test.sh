0425b8c7ac8d467c9bd88f8b4cfd26fa165534fe
Alex Sahm <alexander.sahm@gmx.de>
2016-09-24 17:43:40 +0200

    make test running

--- mylib_test.sh
+++ mylib_test.sh
@@ -6,25 +6,26 @@ filename="mylib_test.sh"
 
 function test_execCmd_validCommand() {
  mn="test_checkRC_validCommand()"
- echo "Test test_execCmd_validCommand..."
+ log $filename $mn "INFO" "Test test_execCmd_validCommand..."
  execCmd "pwd"
  if [ $? -ne 0 ]; then 
-     echo "Error in test $mn. Command must be successfully executed." 
+     log $filename $mn "ERROR" "Error in test. Command must be successfully executed." 
      exit 1
  fi
- echo "$mn ended successfully" 
+ log $filename $mn "INFO" "successfully completed" 
 }
 
 function test_execCmd_InvalidCommand() {
- mn="test_checkRC_InvalidCommand()"
- echo "Test test_execCmd_InvalidCommand..."
+ mn="test_execCmd_InvalidCommand()"
+ log $filename $mn "INFO" "Test test_execCmd_InvalidCommand..."
  execCmd "invalidCommand"
- if [ $? -eq 0 ]; then 
-     echo "Error in test $mn. Command must end with errors" 
-     exit 1
- fi
- echo "$mn ended successfully"
 }
 
 test_execCmd_validCommand
-test_execCmd_InvalidCommand
+out=$(test_execCmd_InvalidCommand)
+echo $out | grep ERROR 
+if [ $? -ne 0 ]; then
+    log $filename "test_execCmd_InvalidCommand" "ERROR" "Error in test. Command must end with error." 
+    exit 1
+fi
+log $filename "test_execCmd_InvalidCommand" "INFO" "successfully completed" 
